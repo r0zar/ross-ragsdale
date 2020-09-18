0a64024a2087af178df1652bba05546437411f81
Ross Ragsdale <ross.ragsdale@gmail.com>
2020-09-18 14:53:38 -0500

    Update details

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,10 +6,9 @@ import emoji from "react-easy-emoji";
 export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
-    <div className="footer-div">
-      <p className="footer-text">{emoji("Made with ❤️ by Saad Pasta")}</p>
-      <p className="footer-text">Theme by <a href="https://github.com/saadpasta/developerFolio">developerFolio</a></p>
-    </div>
+      <div className="footer-div">
+        <p className="footer-text">{emoji("Made with ❤️ by Ross Ragsdale")}</p>
+      </div>
     </Fade>
   );
 }
