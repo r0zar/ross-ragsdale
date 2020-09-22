cc727f052031e954293da842c8e61a083eff727e
Ross Ragsdale <ross.ragsdale@gmail.com>
2020-09-21 23:38:51 -0500

    Update.

--- src/containers/StartupProjects/StartupProject.js
+++ src/containers/StartupProjects/StartupProject.js
@@ -11,24 +11,27 @@ export default function StartupProject() {
 
   return (
     <Fade bottom duration={1000} distance="20px">
-    <div className="main" id="projects">
-      <div>
-        <h1 className="skills-heading">{bigProjects.title}</h1>
-        <p className="subTitle project-subtitle">{bigProjects.subtitle}</p>
-        <div className="startup-projects-main">
-          <div className="startup-project-text">
-            {bigProjects.projects.map(project => {
-              return (
-                <div className="saaya-health-div" onClick={() => openProjectInNewWindow(project.link)}>
-                  <img alt="Saad Working" src={project.image}></img>
-                </div>
-              );
-            })}
+      <div className="main" id="projects">
+        <div>
+          <h1 className="skills-heading">{bigProjects.title}</h1>
+          <p className="subTitle project-subtitle">{bigProjects.subtitle}</p>
+          <div className="startup-projects-main">
+            <div className="startup-project-text">
+              {bigProjects.projects.map((project) => {
+                return (
+                  <div
+                    className="saaya-health-div"
+                    onClick={() => openProjectInNewWindow(project.link)}
+                  >
+                    <img alt="Saad Working" src={project.image}></img>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="starup-project-image"></div>
           </div>
-          <div className="starup-project-image"></div>
         </div>
       </div>
-    </div>
     </Fade>
   );
 }
